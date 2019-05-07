d17c7d56b6f438b0ce62e978123f1afd89477ce5
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-05-07 18:37:46 -0500

    added collision tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 // import TestBackground from "./components/TestBackground";
 // import TestSpeech from "./components/TestSpeech";
 // import TestHUD from "./components/TestHUD.js";
 // import TestWindow from "./components/TestWindow.js";
 // import TestFrame from "./components/TestFrame.js";
 // import TestFloor from "./components/TestFloor.js";
-import Wrapper from "./Wrapper.js";
+import Wrapper from "./components/Wrapper.js";
 
 
 // import TypeTest from './components/TypeTest.js';
@@ -15,18 +15,10 @@ import Wrapper from "./Wrapper.js";
 function App() {
   return (
     <div>
-    <ArrowKeysReact {...ArrowKeysReact.events} />
-      <Wrapper>
-      {/* <TestFrame />
-      <TestHUD />
-      <TestWindow />
-      <TestSpeech />
-      <TestFloor />  */}
-        
-      </Wrapper>
-        {/* <TypeTest /> */}
+      {/* <Wrapper> */}
 
 
+      {/* </Wrapper> */}
     </div>
   );
 }
