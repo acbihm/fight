de738a99b7a923c1040eff661eb8ef6286c12d0e
Alex C. B <43941153+acbihm@users.noreply.github.com>
2019-05-09 21:18:09 -0500

    lots of tests added, organized components

--- src/App3.js
+++ src/App3.js
@@ -3,64 +3,60 @@ import React, { Fragment } from "react";
 // import ReactDOM from "react-dom";
 import KeyEvents from './components/KeyEvents';
 import EnemyTest from "./components/Test/EnemyTest";
-// import HitBoxPunchTest from "./components/Test/HitBoxPunchTest";
-// import HitBoxKickTest from "./components/Test/HitBoxKickTest";
 import WrapperTest from "./components/WrapperTest";
-// import GuardTest from "./components/Test/GuardTest";
 import Image from "./components/Image";
-// import data from "data.json";
-
-
-
-
-
-
 
 class App3 extends React.Component {
   state = {
     x: 0,
     y: 0,
   };
+
   moveLeft = () => {
     this.setState(prevState => {
       this.setState({ x: prevState.x - 10 });
     });
   };
+
   moveRight = () => {
     this.setState(prevState => {
       this.setState({ x: prevState.x + 10 });
     });
   };
+
   moveUp = () => {
     this.setState(prevState => {
       this.setState({ y: prevState.y - 10 });
     });
   };
+
   moveDown = () => {
     this.setState(prevState => {
       this.setState({ y: prevState.y + 10 });
     });
   };
-  moveUpLeft = () => {
-    this.setState(prevState => {
-      this.setState({ y: prevState.y + 10 }, { x: prevState.x - 10 });
-    });
-  };
-  moveUPLeft = () => {
-    this.setState(prevState => {
-      this.setState({ y: prevState.y - 10 }, { x: prevState.x - 10 });
-    });
-  };
-  moveDownLeft = () => {
-    this.setState(prevState => {
-      this.setState({ y: prevState.y - 10 }, { x: prevState.x - 10 });
-    });
-  };
-  moveDownRight = () => {
-    this.setState(prevState => {
-      this.setState({ y: prevState.y - 10 }, { x: prevState.x + 10 });
-    });
-  };
+
+  // moveUpLeft = () => {
+  //   this.setState(prevState => {
+  //     this.setState({ y: prevState.y + 10 }, { x: prevState.x - 10 });
+  //   });
+
+  // };
+  // moveUpLeft = () => {
+  //   this.setState(prevState => {
+  //     this.setState({ y: prevState.y - 10 }, { x: prevState.x - 10 });
+  //   });
+  // };
+  // moveDownLeft = () => {
+  //   this.setState(prevState => {
+  //     this.setState({ y: prevState.y - 10 }, { x: prevState.x - 10 });
+  //   });
+  // };
+  // moveDownRight = () => {
+  //   this.setState(prevState => {
+  //     this.setState({ y: prevState.y - 10 }, { x: prevState.x + 10 });
+  //   });
+  // };
 
   render() {
     return (
@@ -74,10 +70,7 @@ class App3 extends React.Component {
             onDown={this.moveDown}
             onUpLeft={this.moveUpLeft}
           />
-          
           <EnemyTest />
-
-
           <div
             style={{
               position: "absolute",
@@ -92,7 +85,6 @@ class App3 extends React.Component {
             {/* <HitBoxPunchTest />
             <HitBoxKickTest />
             <GuardTest /> */}
-
           </div>
         </WrapperTest>
       </Fragment>
